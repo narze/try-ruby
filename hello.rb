8d7db6adadaf4100fab06da6145d44264baf78d3
Manassarn Manoonchai <manassarn@gmail.com>
2024-06-12 12:50:13 +0700

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,7 +1,10 @@
-# Usage: ruby hello.rb
-
 def hello(name = "World")
   puts "Hello #{name}!"
 end
 
 hello
+
+n = 10
+str = "The quick brown fox jumps over the lazy dog"
+arr = [1,2,3,4]
+hash = { foo: "bar" }
